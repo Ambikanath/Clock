5902bb9d2d90ad2d8ea70f681403f2fa129aa3d8
Ambikanath <150317599+Ambikanath@users.noreply.github.com>
2024-03-15 10:10:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,9 +8,9 @@ function getCurrentTime(){
    let currentMinutes = date.getMinutes();
    let currentSeconds = date.getSeconds();
 
-   handHours.style.transform = rotate(${currentHours * 30 + currentMinutes/2}deg);
-   handMinutes.style.transform = rotate(${currentMinutes * 6 + currentSeconds * 0.1 }deg);
-   handSeconds.style.transform = rotate(${6+ currentSeconds * 6}deg);
+   handHours.style.transform = `rotate(${currentHours * 30 + currentMinutes/2}deg)`;
+   handMinutes.style.transform = `rotate(${currentMinutes * 6 + currentSeconds * 0.1}deg)`;
+   handSeconds.style.transform = `rotate(${6 + currentSeconds * 6}deg)`;
 }
 
 getCurrentTime();
